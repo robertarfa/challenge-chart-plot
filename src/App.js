855991b74cfb4ad33d097760ce8f5e23267ee1f0
Roberta Aguilar <roberta_rfa@hotmail.com>
2021-02-21 13:31:52 -0300

    InputArea

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import InputArea from './components/InputArea/InputArea';
 
 
 const App = () => {
   return (
     <>
       <Header />
-
+      <InputArea />
       <Footer />
     </>
   );
---
+++ src/components/InputArea/InputArea.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { UnControlled as CodeMirror } from 'react-codemirror2';
+
+require('codemirror/lib/codemirror.css');
+require('codemirror/theme/intelie.css');
+require('codemirror/theme/neat.css');
+require('codemirror/mode/javascript/javascript.js');
+
+export default function InputArea() {
+
+  const chartData = ` { type: 'start', timestamp: 1519862400000, select: ['min_response_time', 'max_response_time'], group: ['os', 'browser'] },
+    { type: 'span', timestamp: 1519862400000, begin: 1519862400000, end: 1519862460000 },
+    { type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3 },
+    { type: 'data', timestamp: 1519862400000, os: 'mac', browser: 'chrome', min_response_time: 0.2, max_response_time: 1.2 },
+    { type: 'data', timestamp: 1519862400000, os: 'mac', browser: 'firefox', min_response_time: 0.3, max_response_time: 1.2 },
+    { type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'firefox', min_response_time: 0.1, max_response_time: 1.0 },
+    { type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.2, max_response_time: 0.9 },
+    { type: 'data', timestamp: 1519862460000, os: 'mac', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.0 },
+    { type: 'data', timestamp: 1519862460000, os: 'mac', browser: 'firefox', min_response_time: 0.2, max_response_time: 1.1 },
+    { type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'firefox', min_response_time: 0.3, max_response_time: 1.4 },
+    { type: 'stop', timestamp: 1519862400000 }`
+
+
+
+  return (
+    //https://www.npmjs.com/package/react-codemirror2
+    <CodeMirror
+      value={chartData}
+      options={{
+        mode: 'javascript',
+        theme: 'intelie',
+        lineNumbers: true
+      }}
+      onChange={(editor, data, value) => {
+      }}
+    />
+  )
+}
